6c279b368dd195003d6604c8e40992aac88ffa75
Jordan Tseng <tingchun0131@gmail.com>
2020-05-21 00:22:48 +0800

    完成admin

--- week5/dist/assets/js/all.js
+++ week5/dist/assets/js/all.js
@@ -3,5 +3,9 @@
 console.log('Hello!');
 $(document).ready(function () {// var $linkContent = $('.nav__link').attr('value')
   // console.log($linkContent)
+  $('.user-stateBox').click(function(){
+    $('.user-box__btnBox').toggleClass('active');
+  });
+  
 });
 //# sourceMappingURL=all.js.map
