695f1af9e02fe726f479ee48ea9fc6c4a052cea5
Jordan Tseng <tingchun0131@gmail.com>
2020-05-20 21:31:26 +0800

    上傳dist才對

---
+++ week5/dist/assets/js/all.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log('Hello!');
+$(document).ready(function () {// var $linkContent = $('.nav__link').attr('value')
+  // console.log($linkContent)
+});
+//# sourceMappingURL=all.js.map
