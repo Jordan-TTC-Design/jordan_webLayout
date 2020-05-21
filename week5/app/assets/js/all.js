0cf38fd00ace4bc691203fdadd84a92b3c052611
Jordan Tseng <tingchun0131@gmail.com>
2020-05-21 23:29:12 +0800

    補動效

--- week5/app/assets/js/all.js
+++ week5/app/assets/js/all.js
@@ -3,11 +3,13 @@ console.log('Hello!')
 $(document).ready(() => {
     // var $linkContent = $('.nav__link').attr('value')
     // console.log($linkContent)
-    $('#qusReplyOpen').click(function(){
-        $('.replayBox').addClass('active');
+    $('.user-stateBox').click(function () {
+        $('.user-box__btnBox').toggleClass('active')
     })
-    $('#qusRaplySend').click(function(){
-        $('.replayBox').removeClass('active');
+    $('#qusReplyOpen').click(function () {
+        $('.replayBox').addClass('active')
     })
-    
+    $('#qusRaplySend').click(function () {
+        $('.replayBox').removeClass('active')
+    })
 })
--- week5/dist/assets/js/all.js
+++ week5/dist/assets/js/all.js
@@ -4,6 +4,9 @@ console.log('Hello!');
 $(document).ready(function () {
   // var $linkContent = $('.nav__link').attr('value')
   // console.log($linkContent)
+  $('.user-stateBox').click(function () {
+    $('.user-box__btnBox').toggleClass('active');
+  });
   $('#qusReplyOpen').click(function () {
     $('.replayBox').addClass('active');
   });
