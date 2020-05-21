43fb1cbe99a42a05cc701745705450f48fb74ce6
Jordan Tseng <tingchun0131@gmail.com>
2020-05-21 23:22:31 +0800

    第五週作業完成，但是就是不能ignore ds.store

--- week5/app/assets/js/all.js
+++ week5/app/assets/js/all.js
@@ -3,4 +3,11 @@ console.log('Hello!')
 $(document).ready(() => {
     // var $linkContent = $('.nav__link').attr('value')
     // console.log($linkContent)
+    $('#qusReplyOpen').click(function(){
+        $('.replayBox').addClass('active');
+    })
+    $('#qusRaplySend').click(function(){
+        $('.replayBox').removeClass('active');
+    })
+    
 })
--- week5/dist/assets/js/all.js
+++ week5/dist/assets/js/all.js
@@ -1,11 +1,14 @@
 "use strict";
 
 console.log('Hello!');
-$(document).ready(function () {// var $linkContent = $('.nav__link').attr('value')
+$(document).ready(function () {
+  // var $linkContent = $('.nav__link').attr('value')
   // console.log($linkContent)
-  $('.user-stateBox').click(function(){
-    $('.user-box__btnBox').toggleClass('active');
+  $('#qusReplyOpen').click(function () {
+    $('.replayBox').addClass('active');
   });
-  
+  $('#qusRaplySend').click(function () {
+    $('.replayBox').removeClass('active');
+  });
 });
 //# sourceMappingURL=all.js.map
