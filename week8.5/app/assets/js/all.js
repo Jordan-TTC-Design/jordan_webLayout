27bcbf547420fdf52c91d1d628204214bf597692
Jordan Tseng <tingchun0131@gmail.com>
2020-06-11 00:28:38 +0800

    建立第八週

---
+++ week8.5/app/assets/js/all.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    console.log('hello')
+    // 輪播大圖
+    var swiper = new Swiper('.swiper-container', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+})
