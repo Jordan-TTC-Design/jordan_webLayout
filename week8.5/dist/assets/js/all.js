635e55903f2c39fa055c095e4b9d3905f5b6b878
Jordan Tseng <tingchun0131@gmail.com>
2020-06-11 00:31:16 +0800

    上傳dist

---
+++ week8.5/dist/assets/js/all.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(document).ready(function () {
+  console.log('hello'); // 輪播大圖
+
+  var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
+});
+//# sourceMappingURL=all.js.map
